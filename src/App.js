fdad7ddb7525b5d8ffb166cb8c2ed9f884881753
Sergey <levashov04sergey@gmail.com>
2021-12-11 14:23:11 +0200

    put css for Navbar, setting Links for menu, create pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import Menu from "./components/pages/Menu";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
 
 function App() {
-  return (
-    <div className="App">
-        <Router>
-            <Routes>
-                <Route  path="/" element={<Navbar/>}/>
-
-            </Routes>
-        </Router>
-      <Navbar/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Navbar/>
+                <Routes>
+                    <Route path="/home" element={<Home/>}/>
+                    <Route path="/menu" element={<Menu/>}/>
+                    <Route path="/about" element={<About/>}/>
+                    <Route path="/contact" element={<Contact/>}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Logo from '../images/pizzaLogo.png';
+import '../styles/Navbar.css';
+import {Link} from "react-router-dom";
+import ReorderIcon from "@material-ui/icons/Reorder";
 
 const Navbar = () => {
+    const [openLinks, setOpenLinks] = useState(false);
+
+    const toggleNavbar = () => {
+          setOpenLinks(!openLinks);
+    };
+
     return (
         <div className='navbar'>
-            <div className="leftSide">
+            <div className="leftSide" id={openLinks? "open" : "close"}>
                 <img src={Logo} alt="logo"/>
+                <div className="hiddenLinks">
+                    <Link to="/home">Home</Link>
+                    <Link to="/menu">Menu</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/contact">Contact</Link>
+                </div>
             </div>
             <div className="rightSide">
-
+                <Link to="/home">Home</Link>
+                <Link to="/menu">Menu</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+                <button onClick={toggleNavbar}>
+                    <ReorderIcon/>
+                </button>
             </div>
         </div>
     );
