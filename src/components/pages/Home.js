b36d598f27f5a6464ee38f0b1fdbca81d9743556
Sergey <levashov04sergey@gmail.com>
2021-12-11 14:39:46 +0200

    building Home comp and styling it

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import BannerImage from '../../images/pizza.jpeg';
+import '../../styles/Home.css';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="home">
+            <div
+                className="headerContainer"
+                style={{backgroundImage: `url(${BannerImage}`}}
+            >
+                <h1>Great Ever Pizzeria!</h1>
+                <p>PIZZA TO FIT ANY TASTE</p>
+                <Link to="/menu">
+                    <button>ORDER NOW</button>
+                </Link>
+            </div>
         </div>
     );
 };
