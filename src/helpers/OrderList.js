8c56c44102d81a9f676b6fe2816d245e34f6d5e8
jackine08 <30682208+jackine08@users.noreply.github.com>
2022-11-17 13:28:36 +0900

    add order state done

--- src/helpers/OrderList.js
+++ src/helpers/OrderList.js
@@ -19,4 +19,24 @@ export const OrderList = [
         menu: "Champagne Feast Dinner",
         state: "In Progress",
     },
+    {
+        order_id: 119,
+        menu: "Valentine dinner",
+        state: "Done",
+    },
+    {
+        order_id: 118,
+        menu: "French Dinner",
+        state: "Before",
+    },
+    {
+        order_id: 117,
+        menu: "English Dinner",
+        state: "Done",
+    },
+    {
+        order_id: 116,
+        menu: "Champagne Feast Dinner",
+        state: "In Progress",
+    },
 ];
