e3f39a96557ebabd4c7c9795569ad073d12f78a7
jackine08 <30682208+jackine08@users.noreply.github.com>
2022-11-17 23:16:15 +0900

    add function MenuItem

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,43 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useRef} from 'react'
 import RadioGroup from "../helpers/RadioGroup";
 import Radio from "../helpers/Radio";
 import Counter from "../helpers/Counter";
 const MenuItem = ({image, name, price}) => {
-    function order(){
 
 
-    };
+    const demand = useRef();
+    // const style = useRef();
+    const menu_num = useRef();
+    const wine_num = useRef();
 
     function incart(){
+        var obj = {demand: demand.current.value, style: style, menu_num:menu_num.current.value, wine_num: wine_num.current.value};
 
+        console.log(obj);
+        //axios.post("order/set")
 
-  }
+    };
+    const [style, set_style] = useState("simple")
     return (
         <div className="menuItem">
             <div style={{backgroundImage: `url(${image})`}}></div>
             <h1>{name}</h1>
             <p>${price}</p>
-            <RadioGroup>
-        <Radio name="contact" value="EMAIL">
-          심플
-        </Radio>
-        <Radio name="contact" value="PHONE">
-          그랜드
-        </Radio>
-        <Radio name="contact" value="FAX">
-          디럭스
-        </Radio>
+            <RadioGroup value = {style} onChange={set_style}>
+                {name != "샴페인 축제 디너" && <Radio name="contact" value="simple">
+                  심플
+                </Radio>}
+                <Radio name="contact" value="grand">
+                  그랜드
+                </Radio>
+                <Radio name="contact" value="deluxe">
+                  디럭스
+                </Radio>
+            </RadioGroup>
 
-      </RadioGroup>
-        <p>요청사항
-        <input type="text"></input>
-        </p>
-        수량
-        <Counter />
-        샴페인?
-        <Counter />
-        <input type="button" value="주문하기" onClick={order}></input>
-        <input type="button" value="장바구니 담기" onClick={incart}></input>
+            <p>요청사항
+            <input type="text" ref={demand}></input>
+            </p>
+            <p>
+            수량
+            <input type="text" ref={menu_num} />
+            </p>
+            <p>
+            와인
+            <input type="text" ref={wine_num}/>
+            </p>
+            <input type="button" value="장바구니 담기" onClick={incart}></input>
         </div>
     );
 };
