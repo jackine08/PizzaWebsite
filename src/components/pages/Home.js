421cb0c34169be565e1ba1068be17849fbbd8fab
sok5188 <sok5188@gmail.com>
2022-11-18 11:09:21 +0900

    modified home,navbar "login Check"

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,22 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import BannerImage from "../../images/pizza.jpeg";
 import "../../styles/Home.css";
-
+import axios from "axios";
+const logcheck = async () => {
+  const check = await axios.get("auth/check_login");
+  console.log(check);
+  console.log(check.data);
+  console.log(check.data.islogin);
+  console.log(check.data.name);
+  return check.data;
+};
 const Home = () => {
-  return (
-    <div className="home" style={{ backgroundImage: `url(${BannerImage}` }}>
-      <div className="headerContainer">
-        <Link to="/login">
-          <button>Login</button>
-        </Link>
-        <h1> </h1>
-        <Link to="/Register">
-          <button>Register</button>
-        </Link>
+  let [is_logined, setIs_logined] = useState([]);
+  let [name, setName] = useState([]);
+  const getcheck = logcheck();
+  getcheck.then((res) => {
+    setIs_logined(res.islogin);
+    setName(res.name);
+  });
+  if (is_logined === "True") {
+    return (
+      <div className="home" style={{ backgroundImage: `url(${BannerImage}` }}>
+        Welcome !
+        <div className="headerContainer">
+          <Link to="/menu">
+            <button>Get Order!</button>
+          </Link>
+          <h1> </h1>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="home" style={{ backgroundImage: `url(${BannerImage}` }}>
+        <div className="headerContainer">
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+          <h1> </h1>
+          <Link to="/Register">
+            <button>Register</button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Home;
