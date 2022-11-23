3c356c70c71468fce60b22533e88ce2f537b9ea4
jackine08 <30682208+jackine08@users.noreply.github.com>
2022-11-23 21:39:16 +0900

    Ingredient update

--- src/components/pages/manage/IngredientManage.js
+++ src/components/pages/manage/IngredientManage.js
@@ -1,78 +1,78 @@
-import React, { useState }from 'react';
+import React, { useState, useEffect }from 'react';
 import { useRef } from 'react';
-import {IngredientList} from '../../../helpers/IngredientList';
+import axios from 'axios';
 import '../../../styles/Order.css';
 
 const IngredientManage = () => {
 
-    function get_ingredient_data(id){
-        var contents;
+    var refList = [useRef(), useRef(), useRef(),useRef(), useRef()];
 
-        var obj = {INDEX:0};
-
-        //get ingredients data through post communication
-        // axios
-        //   .post("stock/get", obj)
-        //   .then((res) => {
-        //     contents = res.data;
-        //   })
-        //   .catch((e) => {
-        //     console.error(e);
-        //   });
-        contents = IngredientList
-
-        return contents;
+    function fetch_data(id){
+        console.log("start fetch_data");
+        var obj = {uid:"manager"};
+        var __temp = [];
+        axios
+          .post("stock/get", obj)
+          .then((res) => {
+            console.log(res.data[0]);
+            set_data(res.data[0]);
+          })
+          .catch((e) => {
+            console.error(e);
+          });
+          console.log("end fetch_data");
     };
-
 
     function change_ingredient_count(name, count){
         //change ingredients count through post communication
-
-
-        var obj = {name: name, count: count};
-
-        // axios
-        //   .post("/ingredients/change_count", obj)
-        //   .then((res) => {
-        //     if(res.data==true){
-        //         console.log("change_count");
-        //     }
-        //   })
-        //   .catch((e) => {
-        //     console.error(e);
-        //   });
-
-
+        var obj = {steak: parseInt(refList[0].current.value),
+                    salad: parseInt(refList[1].current.value),
+                    egg: parseInt(refList[2].current.value),
+                    bacon: parseInt(refList[3].current.value),
+                    bread: parseInt(refList[4].current.value)}
+        console.log(obj);
+        console.log("send data to server");
+        axios
+          .post("stock/set", obj)
+          .then((res) => {
+            if (res.data.status == "Success") {
+              console.log("change Done");
+            } else console.log("change Fail");
+          })
+          .catch((e) => {
+            console.error(e);
+          });
+          fetch_data();
     }
 
 
-    const IngredientItem = ({key, name, count}) => {
-
+    const IngredientItem = ({value, name, count}) => {
+        console.log(value);
         return (
-            <div className="orderItem" key = {key}>
+            <div className="orderItem">
                 <h1>{name}</h1>
                 <p>{count}</p>
-                <input type="text" id = {name}  ></input>
-                <input type="button" value = "confirm" onClick={()=>{change_ingredient_count(name, count);}}></input>
+                <input type="text" id = {name} ref = {refList[value]}></input>
             </div>
         );
     };
 
 
-    var [data_to_show, change_data] = useState(get_ingredient_data(132));
+    var [data_to_show, set_data] = useState([]);
+
+    useEffect(()=>{fetch_data();}, []);
 
     return (
         <div className="order">
             <h1 className="orderTitle">Ingredient list</h1>
             <div className="orderList">
-                {data_to_show.map((ingredientItem) => {
-                    return (<IngredientItem
-                            key={ingredientItem.name}
-                            name={ingredientItem.name}
-                            count={ingredientItem.count}/>
-                    );
-                })}
+                <IngredientItem value='0' name="steak" count={data_to_show.steak}/>
+                <IngredientItem value='1' name="salad" count={data_to_show.salad}/>
+                <IngredientItem value='2' name="egg" count={data_to_show.egg}/>
+                <IngredientItem value='3' name="bacon" count={data_to_show.bacon}/>
+                <IngredientItem value='4' name="bread" count={data_to_show.bread}/>
             </div>
+            <button onClick={change_ingredient_count}> change Ingreditne Count </button>
         </div>
     );
 };
