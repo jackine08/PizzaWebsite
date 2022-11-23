e1fbd340d15d833c29ec392c9e31eb74a1ed3a42
jackine08 <30682208+jackine08@users.noreply.github.com>
2022-11-23 21:32:57 +0900

    delivery update

--- src/components/pages/manage/DeliveryManage.js
+++ src/components/pages/manage/DeliveryManage.js
@@ -1,87 +1,147 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom'
-import {DeliveryList} from '../../../helpers/DeliveryList';
-import '../../../styles/Delivery.css';
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import ReactDOM from "react-dom";
+import { OrderList } from "../../../helpers/OrderList";
+import "../../../styles/Order.css";
+import { LocalParking } from "@material-ui/icons";
+import Login from "../Login";
 
 const DeliveryManage = () => {
+  const [data_to_show, set_data] = useState([]);
+  let [order_data, set_order_data] = useState([]);
+  let [order_data_done, set_order_data_done] = useState([]);
+  var contents = [];
+  var view_state = 0;
 
-    var contents
 
-    const [data_to_show, set_data] = useState(get_delivery_data(0));
-    var data_state = 0;
+  function changeState(order_id, state) {
+    var obj = { order_id: order_id, state: state };
+    console.log(obj);
+    const __data_temp = async()=>{
+        try{
+            console.log(obj);
+            const __res = await axios.post("order/modify", obj);
+            var __temp = __res.data;
+            console.log(__res.data);
+            if (__temp.status == "Success") {
+              console.log("delivery change Done");
+          } else console.log("delivery change Fail");
+        } catch (e) {
+          console.error(e.message);
+        }
+    }
+    fetch_data();
+  }
 
-    function get_delivery_data(id){
-        //get delivery using post communication
-        var obj = {id : "Manager"};
-        // axios
-        //   .post("order/get", obj)
-        //   .then((res) => {
-        //     contents = res.data;
-        //   })
-        //   .catch((e) => {
-        //     console.error(e);
-        //   });
-        contents = DeliveryList;
+  const mounted = useRef(false);
 
-        //parsing
-        return contents;
+  const fetch_data = async () => {
+      try {
+        var temp1 = [];
+        var temp2 = [];
+        var obj = { uid: "Manager", state:"" };
+        const res = await axios.post("order/get", obj);
+        // 받아온 데이터를 useState 를 이용하여 선언한다.
+        contents = res.data;
+        console.log("contents: ",contents);
 
+        for (var i = 0; i < contents.length; i++) {
+          if (contents[i].order_status == "cook_Done" || contents[i].order_status == "on_Delivery") {
+            console.log("pushed done:", contents[i]);
+            temp1.push(contents[i]);
+        } else if(contents[i].order_status == "Delivery_done"){
+            console.log("pushed no done:", contents[i]);
+            temp2.push(contents[i]);
+          }
+        }
+        set_order_data(temp1);
+        set_order_data_done(temp2);
+        console.log("now order data : ", temp1);
+        console.log("now done order data", temp2);;
+        set_data(temp1);
+        console.log("set_data_to_show_donw");
+      } catch (e) {
+        console.error(e.message);
+      }
+    console.log("In use Effect");
     };
 
-    function changeState(deliveryNum, state){
-        console.log(state);
-        console.log(deliveryNum);
-        var obj = {order_id : deliveryNum, state : state};
+  useEffect(()=>{fetch_data();}, []);
 
-        // axios
-        //   .post("order/modify", obj)
-        //   .then((res) => {
-        //     if(res.status == "Success"){
-        //         console.log("change Done")
-        //     }
-        //   })
-        //   .catch((e) => {
-        //     console.error(e);
-        //   });
+  function Change_data_to_show(state) {
+    console.log("change_data_to_show");
+    // console.log(order_data);
+    // console.log(order_data_done);
+    if (state == 1) {
+      set_data(order_data_done);
+      console.log("load done data", data_to_show);
+    } else {
+      set_data(order_data);
+      console.log("load not done data ", data_to_show);
+    }
+  }
 
-        get_delivery_data(123);
-        set_data(contents);
-    };
-
-
-    const DeliveryItem = ({key, deliveryman, order_num, state}) => {
-        return (
-            <div className="deliveryItem">
-                <h1>{deliveryman}</h1>
-                <p>{order_num}</p>
-                <p>{state}</p>
-                <input type="button" value = "cook_done" onClick={()=>{changeState(order_num,"cook_done");}}></input>
-                <input type="button" value = "on_delivery" onClick={()=>{changeState(order_num,"on_delivery");}}></input>
-                <input type="button" value = "delivery_done" onClick={()=>{changeState(order_num,"delivery_done");}}></input>
-            </div>
-        );
-    };
-
-
-
-
-    const element = (
-        <div className="delivery">
-            <h1 className="deliveryTitle">Delivery list</h1>
-            <div className="deliveryList">
-                {data_to_show.map((deliveryItem) => {
-                    return (<DeliveryItem
-                            key={deliveryItem.delivery_man}
-                            deliveryman={deliveryItem.deliveryman}
-                            order_num={deliveryItem.order_num}
-                            state={deliveryItem.state} />
-                    );
-                })}
-            </div>
-        </div>
+  //console.log("Point5");
+  const DeliveryItem = ({ key, menu, state, id, change }) => {
+    console.log("in Delivery Item");
+    return (
+      <div className="orderItem">
+        <h1>주문번호: {id}</h1>
+        <p>name: {menu}</p>
+        <p>order_status: {state}</p>
+        <p>change: {change}</p>
+        <input
+          type="button"
+          value="cook_Done"
+          onClick={() => {
+            changeState(id, "cook_Done");
+          }}></input>
+        <input
+          type="button"
+          value="on_Delivery"
+          onClick={() => {
+            changeState(id, "on_Delivery");
+          }}></input>
+        <input
+          type="button"
+          value="Delivery_done"
+          onClick={() => {
+            changeState(id, "Delivery_done");
+          }}></input>
+      </div>
     );
+  };
 
-    return (element);
+  const element = (
+    <div className="order">
+      <h1 className="orderTitle">Delivery list</h1>
+      <input
+        type="button"
+        value="InDelivery"
+        onClick={() => Change_data_to_show(0)}
+      />
+      <input
+        type="button"
+        value="Delivery_Done"
+        onClick={() => Change_data_to_show(1)}
+      />
+      <div className="orderList">
+        {data_to_show.map((orderItem) => {
+          return (
+            <DeliveryItem
+              key={orderItem.order_id}
+              menu={orderItem.menu}
+              state={orderItem.order_status}
+              id={orderItem.order_id}
+              change={orderItem.change_list}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+  //console.log("Point5");
+  return element;
 };
 
 export default DeliveryManage;
